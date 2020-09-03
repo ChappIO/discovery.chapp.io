6a12e229e0c86a583ebd560f639bcb8b86f4b01d
Thomas Biesaart <thomas.biesaart@protonmail.com>
2020-09-03 17:57:21 +0200

    Run tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,7 @@ eval `go env | sed -e 's/^/export /'`
 if [ "${1:-}" = "watch" ]; then
   $GOPATH/bin/goconvey .
 else
-  go test ./...
+  go test -coverprofile coverage.out -json ./... | tee test-report.json
+  go tool cover -html=coverage.out -o coverage.html
 fi
 
